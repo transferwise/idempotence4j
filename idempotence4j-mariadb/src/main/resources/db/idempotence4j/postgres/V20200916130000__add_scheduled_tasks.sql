5d30722e61b574064618d82244163d58afcd3176
Peter Turovskyy <petro.turovksyy@transferwise.com>
2020-09-25 13:09:13 +0300

    migration script sql formatting

--- idempotence4j-mariadb/src/main/resources/db/idempotence4j/postgres/V20200916130000__add_scheduled_tasks.sql
+++ idempotence4j-mariadb/src/main/resources/db/idempotence4j/postgres/V20200916130000__add_scheduled_tasks.sql
@@ -3,13 +3,13 @@ CREATE TABLE IF NOT EXISTS idempotence4j_scheduled_tasks (
     task_instance           VARCHAR(40)     NOT NULL,
     task_data               BLOB,
     execution_time          TIMESTAMP(6)    NOT NULL,
-    picked BOOLEAN          NOT NULL,
+    picked                  BOOLEAN         NOT NULL,
     picked_by               VARCHAR(50),
     last_success            TIMESTAMP(6),
     last_failure            TIMESTAMP(6),
     consecutive_failures    INT,
     last_heartbeat          TIMESTAMP(6),
-    version BIGINT          NOT NULL,
+    version                 BIGINT          NOT NULL,
 
     PRIMARY KEY (task_name, task_instance)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- idempotence4j-postgres/src/main/resources/db/idempotence4j/postgres/V20200915100000__add_scheduled_tasks.sql
+++ idempotence4j-postgres/src/main/resources/db/idempotence4j/postgres/V20200915100000__add_scheduled_tasks.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS idempotence4j_scheduled_tasks (
-    task_name               VARCHAR(40) NOT NULL,
-    task_instance           VARCHAR(40) NOT NULL,
+    task_name               VARCHAR(40)                 NOT NULL,
+    task_instance           VARCHAR(40)                 NOT NULL,
     task_data               BYTEA,
-    execution_time          TIMESTAMP WITH TIME ZONE NOT NULL,
-    picked BOOLEAN          NOT NULL,
+    execution_time          TIMESTAMP WITH TIME ZONE    NOT NULL,
+    picked                  BOOLEAN                     NOT NULL,
     picked_by               VARCHAR(50),
     last_success            TIMESTAMP WITH TIME ZONE,
     last_failure            TIMESTAMP WITH TIME ZONE,
     consecutive_failures    INT,
     last_heartbeat          TIMESTAMP WITH TIME ZONE,
-    version BIGINT          NOT NULL,
+    version                 BIGINT                      NOT NULL,
 
     PRIMARY KEY (task_name, task_instance)
 )
